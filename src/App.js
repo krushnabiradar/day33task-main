6dead4c13b266e2c146608faebd284ffa78dd889
krushnabiradar <krushnabiradar1995@.com>
2022-07-13 11:17:11 +0530

    admin dashboard

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import NavigationDrawer from "./components/NavigationDrawer";
+
+export default function App() {
+  return (
+    <div className="App">
+      <NavigationDrawer />
+    </div>
+  );
+}
